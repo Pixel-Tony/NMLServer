1a820b1bf6691e4223a2efdd6e2464e3af4cf412
Pixel-Tony <ant.peshkoff9@gmail.com>
2024-02-14 21:38:38 +0100

    [chore] Add more information to SymbolKind

--- src/grammar/SymbolKind.cs
+++ src/grammar/SymbolKind.cs
@@ -1,12 +1,20 @@
 namespace NMLServer;
 
+/// <summary>
+/// The enumeration of symbol types for <see cref="NMLServer.Lexing.IdentifierToken"/> token.
+/// </summary>
+[Flags]
 internal enum SymbolKind : byte
 {
     None = 0,
-    Feature,
-    Function,
-    Macro,
-    Variable,
-    Parameter,
-    Constant
+    Feature = 0x01,
+    Switch = 0x02,
+    Macro = 0x04,
+    Variable = 0x05,
+    Parameter = 0x06,
+    Constant = 0x07,
+
+    UserDefined = 0x20,
+    ReadableOutsideSwitch = 0x40,
+    Writeable = 0x80
 }
