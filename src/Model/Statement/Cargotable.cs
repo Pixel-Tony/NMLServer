4eabbc874947b2dd777e28efcb8c7a7b32615135
Anton Pieshkov <ant.peshkoff9@gmail.com>
2025-07-28 00:19:43 +0200

    [fix] cargotable not allowing strings

--- src/Model/Statement/Cargotable.cs
+++ src/Model/Statement/Cargotable.cs
@@ -3,4 +3,4 @@ using NMLServer.Model.Lexis;
 namespace NMLServer.Model.Statement;
 
 internal sealed class Cargotable(ref ParsingState state, KeywordToken keyword)
-    : BlockStatement<ValueWithComma<IdentifierToken>>(ref state, keyword, ParamInfo.None);
+    : BlockStatement<ValueWithComma<BaseValueToken>>(ref state, keyword, ParamInfo.None);
